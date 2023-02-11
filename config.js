472d50f865a7f4ea717b9bb74032c7d4df1298ab
Nguyễn Hoàng Long <74346303+mthlong@users.noreply.github.com>
2023-02-12 02:59:42 +0700

    upload project

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Ann heoooo';
+const giftUrl = '#';
+const eventName = 'Happy valentine day !!!';
+const titleCard = 'Gửi bà chã yêu của anhhh';
+const contentCard = 'Năm nay là valentine thứ 2 cùng với Ann heo gòi đóa. Chỉ muốn thật gần em để có thể chụp với em 1 tấm hình hay ăn 1 miếng bánh, nhưng để hè mình bù nha. Hi vọng năm nào cũng được đón valentine cùng bà chã iu. Yêu em nhiều lắm chúng ta cùng cố gắng để về cùng một mái ấm nhaaaa ^_^ Yêu bà chã nhiều !';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'hot-girl2.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
